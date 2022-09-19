49d4298e563a9330ca8f15a178a5d38fe4a0ebbc
SolDev-HP <develop.hp.block@gmail.com>
2022-09-18 22:21:23 -0400

    Add help,describe,templates,template functionality and placeholder functions for basic logging

--- bagn-o-meme-bot/test.py
+++ bagn-o-meme-bot/test.py
@@ -32,6 +32,9 @@ def meme_template_function():
 def describe_template():
     return 'Template description summoned'
 
+def wrong_command_try_again():
+    return 'Wrong command bitch, try again'
+
 
 class ChooseAction(Enum):
     # Help command, describe how !bagn command works 
@@ -42,11 +45,13 @@ class ChooseAction(Enum):
     templatesfunction = (templates_function,)
     # Main meme command
     memefunction = (meme_template_function,)
+    # Just incase we enter wrong commands 
+    wrongnessfunction = (wrong_command_try_again,)
 
     # Now based on class's enum selection, we'll be able to call corresponding function
     # that's cool 
     def __call__(self, *args, **kwargs):
-        self.value[0](*args, **kwargs)
+        return self.value[0](*args, **kwargs)
 
 # Then we define events, and based on those events, interaction
 @client.event 
@@ -58,44 +63,61 @@ async def on_message(message):
     if message.author == client.user:
         return 
 
+    # If the message content starts with opening_command, enter this and split the received args
     if message.content.startswith(opening_command):
         # Check which command we received 
         received_commands = message.content.split()
+
         # opening_command help|describe|templates|<template_name> [Args, either Optional or required, based on template]
+        # received commands length has to be 2 or more. Ex: !bagn help 
+        if len(received_commands) < 2:
+            await message.channel.send(ChooseAction.wrongnessfunction())
+            return
 
-        # It has to start with !bagn or any other predefined command 
-        # Clear path - if meme.png exists, delete it 
-        if(os.path.exists('./generated_memes/meme.png')):
-            os.remove('./generated_memes/meme.png')
+        # now check second command and call related functions accordingly 
+        if received_commands[1].lower() == 'help':
+            await message.channel.send(ChooseAction.helpfunction())
+        elif received_commands[1].lower() == 'describe':
+            await message.channel.send(ChooseAction.describefunction())
+        elif received_commands[1].lower() == 'templates':
+            await message.channel.send(ChooseAction.templatesfunction())
+        else:
+            # Whatever this is, should be considered potential meme-template name 
+            await message.channel.send(ChooseAction.memefunction())
+
+        # # It has to start with !bagn or any other predefined command 
+        # # Clear path - if meme.png exists, delete it 
+        # if(os.path.exists('./generated_memes/meme.png')):
+        #     os.remove('./generated_memes/meme.png')
         
-        # MemePy expects args, let's provide following and see what happens
-        # template = 'MeAlsoMe'     - Things are case sensetive here 
-        # path = ./generated_memes dir 
-        # args = depending upon the meme template 
+        # # MemePy expects args, let's provide following and see what happens
+        # # template = 'MeAlsoMe'     - Things are case sensetive here 
+        # # path = ./generated_memes dir 
+        # # args = depending upon the meme template 
 
-        # Options = None at this time
-        memeObj = MemePy.save_meme_to_disk(
-            'MeAlsoMe',
-            './generated_memes', [
-                'Flip burgers',
-                'into Bagners'
-            ]
-        )
+        # # Options = None at this time
+        # memeObj = MemePy.save_meme_to_disk(
+        #     'MeAlsoMe',
+        #     './generated_memes', [
+        #         'Flip burgers',
+        #         'into Bagners'
+        #     ]
+        # )
 
-        # As MemePy creates meme.png file, we need to rename it to something 
-        # we take that filename that's eventually be passed into discord.File() 
-        # Later we can use this to delete the temp file that we created - Depends, do we wanna keep them?
-        memefilename = './generated_memes/meme.png'
+        # # As MemePy creates meme.png file, we need to rename it to something 
+        # # we take that filename that's eventually be passed into discord.File() 
+        # # Later we can use this to delete the temp file that we created - Depends, do we wanna keep them?
+        # memefilename = './generated_memes/meme.png'
 
-        # Now it requires two more things, depending upon which meme template is selected 
-        # you need text args 
-        #- ----- Scratch that and now get file object from discord 
-        # need an fp?
-        fp = open(memefilename, 'rb')
-        discfile = discord.File(fp=fp, filename=memefilename, spoiler=False, description='Return of the meme')
-        fp.close()
-        # Returning result would be an image 
-        await message.channel.send('Here ya go cowboy', file=discfile)
+        # # Now it requires two more things, depending upon which meme template is selected 
+        # # you need text args 
+        # #- ----- Scratch that and now get file object from discord 
+        # # need an fp?
+        # fp = open(memefilename, 'rb')
+        # discfile = discord.File(fp=fp, filename=memefilename, spoiler=False, description='Return of the meme')
+        # fp.close()
+        # # Returning result would be an image 
+        # await message.channel.send('Here ya go cowboy', file=discfile)
 
     if 'bitch' in message.content:
         await message.channel.send('Wooaahh there cowboy. Eeassyy there!')
